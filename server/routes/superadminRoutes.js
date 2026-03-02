2263c6b6e9f98c2de1d5d13b7fff34880bdce7b9
tejaskhope21 <tejaskhope600@gmail.com>
2026-03-02 11:28:58 +0530

    push for backend

--- server/routes/superadminRoutes.js
+++ server/routes/superadminRoutes.js
@@ -1,7 +1,7 @@
 // routes/superAdminRoutes.js
 import express from 'express';
 import { protect, superAdminOnly } from '../middleware/auth.js';
-import { createAdmin } from '../controllers/superadmincontroller.js';
+import { createAdmin } from '../controllers/superadminController.js';
 
 const router = express.Router();
 
